b6ed0b5065a44376ee5843873810c316e7150448
fmarcelomorais <marcelomorais.program@gmail.com>
2023-12-28 22:42:24 -0300

    ajusta url

--- src/service/endpoint.js
+++ src/service/endpoint.js
@@ -1,4 +1,4 @@
-const vercel_url = 'https://api-erp-thstreaming.vercel.app/'
+const vercel_url = 'https://api-erp-thstreaming.vercel.app'
 //const vercel_url = 'http://localhost:3333'
 const swal = require("sweetalert")
 
